975d3d9f25e0fc45700c5b86291355b1a2caba7e
Tate Shepard <tateshepard@yahoo.com>
2019-08-05 00:47:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,8 +16,8 @@ def get_user_input
   gets.chomp.strip
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
