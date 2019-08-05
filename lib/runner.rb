2415dc929fa107c20cc5048be5bfcb5d7ce9c803
Tate Shepard <tateshepard@yahoo.com>
2019-08-05 00:53:55 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,9 @@
-require_relative "blackjack.rb"
-
-runner
+def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+end
